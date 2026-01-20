1c39bc435f6783012f8fb9a92e35a1358a905a07
nour-ha123 <hanafi.nour11@gmail.com>
2026-01-20 19:13:03 +0100

    probleme5

--- probleme5.js
+++ probleme5.js
@@ -3,7 +3,7 @@
 // 1- Crée une fonction qui affiche les nombres de 0 à 10 avec while. (console.log()) :) 
 function nombres() {
     var counter = 0;
-    while (counter >= 10) {
+    while (counter <= 10) {
         console.log(counter);
         counter = counter + 1;
     }
