44d8d85f510283dbeb82763da9063f1c728d37bd
imen-mekni <113175332+imen-mekni@users.noreply.github.com>
2026-01-29 18:16:59 +0100

    initial commit

---
+++ probleme9.js
@@ -0,0 +1,19 @@
+// 1- Crée une fonction qui prend un tableau et retourne le nombre d’éléments pairs avec une boucle while.
+
+
+
+// 2- Crée une fonction qui prend un tableau et retourne un nouveau tableau contenant les éléments en ordre inverse avec une boucle for.
+
+
+
+// 3- Crée une fonction qui prend un tableau et retourne un nouveau tableau contenant seulement les nombres impairs avec une boucle while.
+
+
+
+// 4- Crée une fonction qui prend un tableau et retourne le nombre d’éléments positifs avec une boucle while.
+
+
+
+// 5- Crée une fonction qui prend un tableau et retourne un nouveau tableau contenant les éléments à l’index pair avec une boucle for.
+
+
