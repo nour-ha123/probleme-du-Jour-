69c869f59d6b4625844d495cd3241ee726d09052
nour-ha123 <hanafi.nour11@gmail.com>
2026-02-28 10:58:51 +0100

    probleme16

--- probleme16.js
+++ probleme16.js
@@ -3,6 +3,8 @@
  cafe : une propriété représentant le nombre de cafés que votre café peut servir.
  petitGateau : une propriété représentant le nombre de petits gâteaux que votre café peut servir.
 
+ 
+
  Créez une fonction interne servirClient qui sert du café et des petits gâteaux au nombre de clients passé en paramètre.
 */
 
@@ -11,3 +13,20 @@
 // starBucks(5)  => "Nous avons servi 5 clients et il reste 15 cafés et 10 petits gâteaux"
 // starBucks(10) => "Nous avons servi 10 clients et il reste 5 cafés et 0 petits gâteaux"
 // starBucks(3)  => "Nous n'avons plus de petits gâteaux"
+
+function creerCafe (cafe, petitGateau) {
+    return {
+        servirClient:  function(clients) {
+            if(petitGateau === 0 ) {
+                return "Nous n'avons plus de petits gâteaux"
+            }
+
+            cafe = cafe - clients
+            petitGateau = petitGateau - clients
+
+            if(cafe < 0 || petitGateau < 0){
+                return "Nous avons servi " + clients + " cliens et il reste " + cafe + " cafés et " + "petits gâteaux"
+            }
+        }
+    }
+}
