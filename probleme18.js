99ce5927efe7f143f978a362428038b14578487c
nour-ha123 <hanafi.nour11@gmail.com>
2026-03-23 19:20:48 +0100

    probleme18

--- probleme18.js
+++ probleme18.js
@@ -6,6 +6,25 @@
  * var onlyEven = evenOccurrence([1, 7, 2, 4, 5, 6, 8, 9, 6, 4]);
  * console.log(onlyEven); // 4
  */
+
+etape 1 : on ajoute un tableau Vide
+etape 2 : boucle for pour parcourir le tableau
+etape 3 : on ajoute un counter vide 
+etape 4 : on calcul le nombre de repetition pour chaque element 
+etape 5 : on ajout if pour voir si le counter est pair 
+etape 6 : on retourn null s'il ya aucun nombre
+
 var evenOccurrence = function(arr) {
-  // Votre code ici.
+  for (var i = 0; i < arr.length; i++) {
+    var count = 0;
+    for(var j = 0; j <arr.length; j++) {
+      if(arr[i] === arr[j]) {
+        count = count + 1;
+      }
+    }
+    if (count % 2 === 0) {
+      return arr[i]
+    } 
+  }
+  return null
 };
